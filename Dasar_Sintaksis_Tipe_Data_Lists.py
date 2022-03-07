5a9008fb08deeca04e7dfa9c465405e4a8525748
hp <sahalnurdin@gmail.com>
2022-03-07 19:41:44 +0700

    Dasar Tipe Data List 1

--- Dasar_Sintaksis_Tipe_Data_Lists.py
+++ Dasar_Sintaksis_Tipe_Data_Lists.py
@@ -31,5 +31,47 @@ daftar_buku.clear()
 for i in range (0, len(daftar_buku)):
     print(daftar_buku[i])
 
+print('\nGanti elemen pertama')
+daftar_buku = ['Neural Networks', 'Difficult Riddles', 'Atomic Habits', 'Filosofi Teras']
+daftar_buku[0]= 'Clean Code'
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nAmbil elemen ke-2')
+buku = daftar_buku.pop(1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nBuku yang diambil tadi')
+print(buku)
+
+print('\nPop= Mengambil elemen yang paling ujung')
+daftar_buku.pop()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+# Digunakan pada saat tipe atau struktur data stack untuk optimasi data dan kecepatan program
+# Push = append pada struktur data stack
+print('\nPop Pop-1')
+daftar_buku = ['Neural Networks', 'Difficult Riddles', 'Atomic Habits', 'Filosofi Teras']
+daftar_buku.pop(-1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPop Pop-2')
+daftar_buku = ['Neural Networks', 'Difficult Riddles', 'Atomic Habits', 'Filosofi Teras']
+daftar_buku.pop(-2)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPop Pop-3')
+daftar_buku = ['Neural Networks', 'Difficult Riddles', 'Atomic Habits', 'Filosofi Teras']
+daftar_buku.pop(3)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+
+
+
 
 
