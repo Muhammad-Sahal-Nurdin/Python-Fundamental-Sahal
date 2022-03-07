8e62d1d175a46f1bafe6ba8dd81ed24dcc25b6eb
hp <sahalnurdin@gmail.com>
2022-03-07 21:34:05 +0700

    Dictionary

---
+++ Dasar_Sintaksis_Dictionary.py
@@ -0,0 +1,15 @@
+# Simbol dictionary ditandai dengan {}
+# Harus dalam key
+# Tidak boleh menggunakan angka
+users = {
+    'id': 1,
+    'name': 'Leanne Graham',
+    'username': 'Bret',
+    'email': 'Sincere@april.biz'
+}
+print(users)
+# Untuk memnaggil dictionary tertentu
+print(users['id'])
+print(users['name'])
+print(users['username'])
+print(users['email'])
