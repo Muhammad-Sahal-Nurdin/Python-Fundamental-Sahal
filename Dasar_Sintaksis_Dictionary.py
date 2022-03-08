5471320e10644c591b6e56ad83a7908393dabd32
hp <sahalnurdin@gmail.com>
2022-03-09 06:52:42 +0700

    Dictionary Complex

--- Dasar_Sintaksis_Dictionary.py
+++ Dasar_Sintaksis_Dictionary.py
@@ -2,14 +2,21 @@
 # Harus dalam key
 # Tidak boleh menggunakan angka
 users = {
-    'id': 1,
-    'name': 'Leanne Graham',
-    'username': 'Bret',
-    'email': 'Sincere@april.biz'
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz"
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+    }
 }
 print(users)
 # Untuk memnaggil dictionary tertentu
-print(users['id'])
-print(users['name'])
-print(users['username'])
-print(users['email'])
+print(users["id"])
+print(users["name"])
+print(users["username"])
+print(users["email"])
+print(users["address"][street])
