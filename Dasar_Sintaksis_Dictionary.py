772ca7251bee392af04a88a70471aa349b554ac8
hp <sahalnurdin@gmail.com>
2022-03-09 09:12:12 +0700

    Finish!

--- Dasar_Sintaksis_Dictionary.py
+++ Dasar_Sintaksis_Dictionary.py
@@ -1,17 +1,18 @@
+import json
 # Simbol dictionary ditandai dengan {}
 # Harus dalam key
 # Tidak boleh menggunakan angka
 users = {
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz"
-    "address": {
-        "street": "Kulas Light",
-        "suite": "Apt. 556",
-        "city": "Gwenborough",
-        "zipcode": "92998-3874",
-    }
+    'id': 1,
+    'name': 'Leanne Graham',
+    'username': 'Bret',
+    'email': 'Sincere@april.biz'
+    # 'address': {
+    # 'street': 'Kulas Light',
+    # 'suite': 'Apt. 556',
+    # 'city': 'Gwenborough',
+    # 'zipcode': '92998-3874',
+    # }
 }
 print(users)
 # Untuk memnaggil dictionary tertentu
@@ -19,4 +20,14 @@ print(users["id"])
 print(users["name"])
 print(users["username"])
 print(users["email"])
-print(users["address"][street])
+# print(users["address"][street])
+
+print(users)
+print(type(users))
+print("\nUbah Dictionary ke JSON")
+result = json.dumps(users)
+print(result)
+print(type(result))
+
+with open('result.json', 'w') as file:
+    json.dump(users, file)
